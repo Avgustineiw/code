15cac681f2f83c6c84393117767da95c7c06ecf8
Avgustineiw <z.zinkin@mail.ru>
2024-01-21 23:07:06 +0300

    Domino piling done

---
+++ cpp/cpp practice/codeforces/domino piling/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdlib.h>
+
+typedef long  s64;
+typedef int   s32;
+typedef short s16;
+typedef char  s8;
+typedef unsigned long  u64;
+typedef unsigned int   u32;
+typedef unsigned short u16;
+typedef unsigned char  u8;
+
+int main()
+{
+  u16 m, n;
+
+  std::cin >> m >> n;
+
+  std::cout << (m * n) / 2;
+
+  return 0;
+}
