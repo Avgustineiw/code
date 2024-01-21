58210f2f0714c4603b20cfc58706d0f052d30a40
Avgustineiw <z.zinkin@mail.ru>
2024-01-21 22:58:02 +0300

    team done

--- cpp/cpp practice/codeforces/team/main.cpp
+++ cpp/cpp practice/codeforces/team/main.cpp
@@ -13,24 +13,19 @@ typedef unsigned char  u8;
 
 int main()
 {
-  u16 n;
-  u16 res;
-  u16 temp_in;
-  int temp_res[3];
-  
-  res = 0;
+  u16 n, res(0), Petya, Vasya, Tonya;
+
   std::cin >> n;
 
-  for (u16 i = 0; i < n; i++)
-  {
-    // std::fill_n(temp_res, 3, 0);
+  for (u16 i = 0; i < n; i++) {
+    std::cin >> Petya >> Vasya >> Tonya;
 
-    for (u16 j = 0; j < 3; j++)
-    {
-      temp_in = 0;
-      std::cin >> temp_in;
-      temp_res[i] = temp_in;
+    if (Petya + Vasya + Tonya > 1) {
+      res += 1;
     }
-    std::cout << temp_res[0] << " " << temp_res[1] << " " << temp_res[2];
   }
+  std::cout << res << std::endl;
+
+  return 0;
 }
+
