8ac00d305704b71785569091dbf4f67d46042522
Avgustineiw <z.zinkin@mail.ru>
2024-01-30 13:38:04 +0300

    Intro to pointers

---
+++ cpp/cpp homework/Random classwork/Intro to pointers/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+int main()
+{
+  int x = 35, y = 15, temp = x;  
+
+  int* t_1 = &x;
+  int* t_2 = &y;
+
+  *t_1 = y;
+  *t_2 = temp;
+
+  std::cout << x << " " << y;
+
+  return 0;
+}
+
+  
