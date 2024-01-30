b67fa9b4f884b6aa6f6a1b16c8a0a65b1ae88c74
Avgustineiw <z.zinkin@mail.ru>
2024-01-30 14:27:35 +0300

    Intro to pointers 3

--- cpp/cpp homework/informatics/Maximum element/main.cpp
+++ cpp/cpp homework/informatics/Maximum element/main.cpp
@@ -2,11 +2,22 @@
 #include <stdlib.h>
 #include <string>
 
+int arr[10000];
+
 int main()
 {
-  int n;
+  int i = 0, pos = 0, maxx = 0;
 
-  while (std::cin >> n)
+  while (std::cin >> *(arr + i)) {  
+    if (i == 0 || maxx < *(arr + i)) {
+      maxx = *(arr + i);
+      pos = i;
+    }
+
+    i++;
+  }
+  
+  std::cout << maxx << " " << pos;
 
   return 0;
 }
