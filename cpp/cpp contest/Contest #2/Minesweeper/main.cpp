54226ade33ca21c5ef314dc05b16373d82831271
Avgustineiw <z.zinkin@mail.ru>
2024-02-03 11:49:08 +0300

    I hate minesweeper, but feels good to solve it

--- cpp/cpp contest/Contest #2/Minesweeper/main.cpp
+++ cpp/cpp contest/Contest #2/Minesweeper/main.cpp
@@ -3,41 +3,86 @@
 #include <algorithm>
 #include <string>
 
-void readBombs(char arr[], int bombs, int new_line)
+void readBombs(char **field, int bombs)
 {
+  int bomb_row, bomb_col;
 
+  for (int i = 0; i < bombs; i++) {
+    std::cin >> bomb_row >> bomb_col;
+
+    field[bomb_row-1][bomb_col-1] = '*';
+  }
 }
 
-void addClues(char arr[], int size, int new_line)
-{
 
+int BombNearby(int pos_row, int pos_col, int row, int col, char **field)
+{
+  int nearby[8][2] = {{pos_row+1, pos_col},
+                       {pos_row, pos_col+1},
+                       {pos_row+1, pos_col+1},
+                       {pos_row-1, pos_col},
+                       {pos_row, pos_col-1},
+                       {pos_row-1, pos_col-1},
+                       {pos_row+1, pos_col-1},
+                       {pos_row-1, pos_col+1}};
+  int res = 0;
+
+  for (int i = 0; i < 8; i++) {
+    if ((0 <= nearby[i][0]  && nearby[i][0] < row)&&(0 <= nearby[i][1] && nearby[i][1] < col)) {
+      if (field[nearby[i][0]][nearby[i][1]] == '*') {
+        res++;
+      }
+    }
+  }
+  
+  return res;
 }
 
-void printField(char arr[], int size, int new_line)
-{
 
+void addClues(int row, int col, char **field)
+{
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < col; j++) {
+      if (field[i][j] != '*') {
+        field[i][j] = char(int(field[i][j] + BombNearby(i, j, row, col, field)));
+      }
+    }
+  }
 }
+
+
+void printField(int row, int col, char **field)
+{
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < col; j++) {
+      std::cout << field[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
+}
+
 
 int main()
 {
-  int x, y, bombs;
+  int row, col, bomb;
 
-  std::cin >> y >> x >> bombs;
+  std::cin >> row >> col >> bomb;
 
-  char field[y][x];
+  char **field = new char*[row];
 
-  for (int i = 0; i < x; i++)
-  {
-    for (int j = 0; j < y; j++)
-    {
+  for (int i = 0; i < row; i++) {
+    field[i] = new char[col];
+  }
+
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < col; j++) {
       field[i][j] = '0';
     }
   }
   
-
-  readBombs(field, bombs, x);
-  addClues(field, y*x, x);
-  printField(field, y*x, x);
+  readBombs(field, bomb);
+  addClues(row, col, field);
+  printField(row, col, field);
 
   return 0;
 }
