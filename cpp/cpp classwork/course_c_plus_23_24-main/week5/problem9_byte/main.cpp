9cce909050d7ecb0813d4aa75089e7c336629438
Avgustineiw <z.zinkin@mail.ru>
2024-02-08 22:01:02 +0300

    Clean folders

---
+++ cpp/cpp classwork/course_c_plus_23_24-main/week5/problem9_byte/main.cpp
@@ -0,0 +1,28 @@
+///////////////////////////////////////////////////////////////////////////////
+/// \file
+/// \version    0.1.0
+/// \date       30.01.2024
+///
+/// TASK DESCRIPTION
+///
+/// Write a program to copy a binary file to another file in reverse byte order.
+///////////////////////////////////////////////////////////////////////////////
+#include <fstream>
+#include <iostream>
+
+void reverseCopyBinaryFile(const std::string& inputFilePath, const std::string& outputFilePath)
+{
+  std::ifstream in(inputFilePath);
+  std::ofstream out(outputFilePath);
+
+  while (!inputFilePath.eof()) {
+    
+  }
+}
+
+
+int main() {
+  reverseCopyBinaryFile("input_binary.bin", "output_reversed.bin");
+  
+  return 0;
+}
